02e4c6844ca3fd2537740b4b76068ac7cf8cad15
MtnDewritos <seb@gieser.se>
2017-10-10 16:18:13 +0200

    okay so I added some stuff but my old changes dissapeared
    what is going on I'm gonna try some stuff now

---
+++ src/rougelikeLibrary/Enemy.java
@@ -0,0 +1,11 @@
+package rougelikeLibrary;
+
+public class Enemy extends LivingObject {
+
+	public Enemy(int speed, int health){
+		super(speed, health);
+		
+	}
+	
+	
+}
--- src/rougelikeLibrary/Player.java
+++ src/rougelikeLibrary/Player.java
@@ -1,42 +1,10 @@
 package rougelikeLibrary;
 
-public class Player {
+public class Player extends LivingObject {
 
-	private int health;
-	private int speed;
+	
 	
 	public Player(int speed, int health){
 		
-		if(speed < 0 || health < 0){
-			
-			throw new IllegalArgumentException("Speed and health both need to be 0 or more");			
-		}
-		else{
-		this.speed = speed;
-		this.health = health;
-		}
-	}
-	
-	
-	
-	public void takeDamage(int damge) {
-		if(damge < 0) throw new IllegalArgumentException("Can't take negativ damge.");
-		health -= damge;
-	}
-
-  
-  public int getSpeed(){
-		
-		return speed;
-		
-	}
-  
-	public int getHealth(){
-		
-		return health;
-	}
-	
-	public void setHealth(int health) {
-		this.health = health;
 	}
 }
