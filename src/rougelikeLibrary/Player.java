50fbdb2bbb173ddf928ceda92ebeb7f0a70bdeb3
Benaveld <Benaveld@gmail.com>
2017-10-10 13:42:36 +0200

    Add Player Damage * Added player damage method and tests * Added setHealt to player Benjamin Avery

--- src/rougelikeLibrary/Player.java
+++ src/rougelikeLibrary/Player.java
@@ -2,14 +2,22 @@ package rougelikeLibrary;
 
 public class Player {
 
-	public int health = 0;
+	private int health = 0;
+	
 	
 	public Player(){}
+	
+	public void takeDamage(int damge) {
+		if(damge < 0) throw new IllegalArgumentException("Can't take negativ damge.");
+		health -= damge;
+	}
 	
 	public int getHealth(){
 		
 		return health;
 	}
 	
-	
+	public void setHealth(int health) {
+		this.health = health;
+	}
 }
--- tests/rougelikeLibrary/PlayerTests.java
+++ tests/rougelikeLibrary/PlayerTests.java
@@ -12,6 +12,26 @@ public class PlayerTests {
 		
 	}
 	
+	@Test
+	public void playerDamageTest() {
+		Player p = new Player();
+		p.setHealth(100);
+		p.takeDamage(10);
+		assertEquals(90, p.getHealth());
+	}
 	
+	@Test
+	public void player0Damage() {
+		Player p = new Player();
+		p.setHealth(100);
+		p.takeDamage(0);
+	}
+	
+	@Test(expected = IllegalArgumentException.class)
+	public void playerNagativDamage() {
+		Player p = new Player();
+		p.setHealth(100);
+		p.takeDamage(-10);
+	}
 	
 }
