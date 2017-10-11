fc983bde142a2705226a97f0eb889b50151e741a
benaveld <1345337+benaveld@users.noreply.github.com>
2017-10-11 17:07:47 +0200

    Update Player.java

--- src/rougelikeLibrary/Player.java
+++ src/rougelikeLibrary/Player.java
@@ -1,6 +1,6 @@
 package rougelikeLibrary;
 
-public class Player extends LivingObject {https://github.com/benaveld/INTE_Roglike/pull/10/conflict?name=src%252FrougelikeLibrary%252FPlayer.java&ancestor_oid=ff4d697f54af6eab1785cde8cff7606fbf4732da&base_oid=e53339e7fe68426ed7c59e2665833b08b39bdc4a&head_oid=d06ae1d9a81903bca59bf3d342e1008a3a5aad58
+public class Player extends LivingObject {
 
 	private int posX;
 	private int posY;
