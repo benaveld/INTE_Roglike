412035e402c9d56414ef7cedae003e5ffb4cd71f
Jarvitalo <sebastian@lowtech.nu>
2017-10-12 13:26:04 +0200

    Updatet comments

--- src/rougelikeLibrary/RoomSpace.java
+++ src/rougelikeLibrary/RoomSpace.java
@@ -1,7 +1,7 @@
 package rougelikeLibrary;
 
 /**
- * Handles local space in a room that is relative to the world space.
+ * Handles local space in a room that is relative to the world space. Every coordinate in room space can contain 1 item/characters/door.
  */
 public class RoomSpace {
 }
