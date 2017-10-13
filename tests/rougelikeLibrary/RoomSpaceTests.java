5888c87d4970bdce459432d4a0c709b0ab1c7f74
Jarvitalo <sebastian@lowtech.nu>
2017-10-13 03:16:51 +0200

    Basic coverage for tests. Overlapps with MapTests. Should be merged and refactored.

--- tests/rougelikeLibrary/RoomSpaceTests.java
+++ tests/rougelikeLibrary/RoomSpaceTests.java
@@ -6,4 +6,12 @@ import org.junit.*;
 /**
  */
 public class RoomSpaceTests {
+
+    @Test
+    public void testDimensions() {
+        RoomSpace roomSpace = new RoomSpace(43, 64);
+        assertEquals(roomSpace.getWidth(), 43);
+        assertEquals(roomSpace.getHeight(), 64);
+    }
+
 }
