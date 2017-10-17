5b7b039fccdfde5246286cc5a7776765a5e1e54c
Jarvitalo <sebastian@lowtech.nu>
2017-10-17 16:13:38 +0200

    First test cases

---
+++ tests/rougelikeLibrary/MappableTypeWrapperTests.java
@@ -0,0 +1,38 @@
+package rougelikeLibrary;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class MappableTypeWrapperTests {
+
+    @Test
+    public void testQuantity() {
+        Class [] parameterTypes = {Object.class};
+        Object [] parameterValues = {"test"};
+
+        MappableTypeWrapper mappableTypeWrapper =
+                new MappableTypeWrapper(
+                        String.class,
+                        parameterTypes,
+                        parameterValues,
+                        new EnemyAI(21),
+                        1,
+                        4,
+                        50);
+        assertNotNull(mappableTypeWrapper);
+        assertFalse(mappableTypeWrapper.isMinQuantity());
+        assertFalse(mappableTypeWrapper.isMaxQuantity());
+        mappableTypeWrapper.addQuantity();
+        assertTrue(mappableTypeWrapper.isMinQuantity());
+        assertFalse(mappableTypeWrapper.isMaxQuantity());
+        mappableTypeWrapper.addQuantity();
+        assertFalse(mappableTypeWrapper.isMaxQuantity());
+        mappableTypeWrapper.addQuantity();
+        assertFalse(mappableTypeWrapper.isMaxQuantity());
+        mappableTypeWrapper.addQuantity();
+        assertTrue(mappableTypeWrapper.isMaxQuantity());
+        mappableTypeWrapper.addQuantity();
+        assertTrue(mappableTypeWrapper.isMaxQuantity());
+    }
+}
