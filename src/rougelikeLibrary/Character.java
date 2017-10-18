62a9bf236ff41ad105fb1fd737a94d2cb9d9e1bd
Benaveld <benaveld@gmail.com>
2017-10-18 11:41:06 +0200

    Fixing bugs * Fixed a error with doubliget methods in Room. * Some genral removeing of warings * Fixed imports

--- src/rougelikeLibrary/Character.java
+++ src/rougelikeLibrary/Character.java
@@ -1,9 +1,6 @@
 package rougelikeLibrary;
 
 
-import java.util.HashMap;
-import java.util.List;
-
 public class Character implements Mappable {
 	private Position pos;
 	private int speed;
--- tests/rougelikeLibrary/GameTest.java
+++ tests/rougelikeLibrary/GameTest.java
@@ -8,7 +8,6 @@ import rougelikeLibrary.Position.CardinalDirection;
 
 import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Random;
 
 
